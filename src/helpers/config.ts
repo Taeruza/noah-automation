dc0ba54e364af3ac67fd29f3e8adf7d71c651049
Taier Khvaira <tkhvaira@deloitte.com>
2023-07-08 13:15:45 +0300

    config update

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -6,7 +6,7 @@ const browserOptions: LaunchOptions = {
     'media.navigator.streams.fake': true,
     'media.navigator.permission.disabled': true,
   },
-  headless: false,
+  headless: true,
 };
 process.env.USE_ALLURE = '1';
 
